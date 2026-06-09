be7a172347b6cb99f78f28a81390f7cd957e346d
player <playerc@msn.cn>
2026-06-09 19:48:59 +0800

    change FATAL log to stderr.

--- src/log.c
+++ src/log.c
@@ -6,43 +6,49 @@
 void
 c4mk_log_printf(int level, const char *fname, int lno, const char *fmt, ...)
 {
-	const char *lstr = NULL;
-	va_list args;
+        const char *lstr = NULL;
+        FILE *outf = stdout;
+        va_list args;
 
-	switch (level) {
-	case C4MK_LOG_DEBUG:
-		lstr = "###";
-		break;
-	case C4MK_LOG_INFO:
-		lstr = "+++";
-		break;
-	case C4MK_LOG_WARN:
-		lstr = "***";
-		break;
-	case C4MK_LOG_FATAL:
-		lstr = "!!!";
-		break;
-	default:
-		lstr = "???";
-		break;
-	}
+        switch (level) {
+        case C4MK_LOG_DEBUG:
+                lstr = "###";
+                outf = stdout;
+                break;
+        case C4MK_LOG_INFO:
+                lstr = "+++";
+                outf = stdout;
+                break;
+        case C4MK_LOG_WARN:
+                lstr = "***";
+                outf = stdout;
+                break;
+        case C4MK_LOG_FATAL:
+                lstr = "!!!";
+                outf = stderr;
+                break;
+        default:
+                lstr = "???";
+                outf = stdout;
+                break;
+        }
 
-	printf("[%s] ", lstr);
+        fprintf(outf, "[%s] ", lstr);
 
-	switch (level) {
-	case C4MK_LOG_INFO:
-	case C4MK_LOG_WARN:
-	default:
-		break;
-	case C4MK_LOG_DEBUG:
-	case C4MK_LOG_FATAL:
-		printf("loc:[%s:%d] ", fname, lno);
-		break;
-	}
+        switch (level) {
+        case C4MK_LOG_INFO:
+        case C4MK_LOG_WARN:
+        default:
+                break;
+        case C4MK_LOG_DEBUG:
+        case C4MK_LOG_FATAL:
+                fprintf(outf, "loc:[%s:%d] ", fname, lno);
+                break;
+        }
 
-	va_start(args, fmt);
-	vprintf(fmt, args);
-	va_end(args);
+        va_start(args, fmt);
+        vfprintf(outf, fmt, args);
+        va_end(args);
 
-	fflush(stdout);
+        fflush(outf);
 }
